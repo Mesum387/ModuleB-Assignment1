eae8c78590bc869a8b24f3abee82a43fd20e2d54
Mesum387 <syedmesum387@gmail.com>
2022-03-29 05:45:58 -0700

    AssignmentDone

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
-let obj = [
-    {
-    name:"Mesum",
-    contact:"3242342332",
-    institute:"skhakjsHK"
-  },
-  {
-    name:"HAMZA",
-    contact:"3223423243",
-    institute:"QUWASJKDH"
-  },{
-    name:"HASNAIN",
-    contact:"2342344423",
-    institute:"HJWRUW"
-  },{
-    name:"ALI",
-    contact:"3827643334",
-    institute:"LSJFJ"
-  },
-];
-
-let myobj = obj.map(function(x){
-    return x.name;
-    
-});
 
 
-console.log(myobj);
+let myobject = {
+  name:"Mesum",
+  institute:"Jawan Pakistan",
+  contact:65765657
+}
+let myobjkeys=Object.values(myobject);
+let myobjvalues=Object.keys(myobject);
+console.log(myobjvalues,myobjkeys);
