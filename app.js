ebc4b36acc0e08d1cf56bb654b79dbc47a1ae672
Mesum387 <syedmesum387@gmail.com>
2022-03-29 03:29:13 -0700

    done

---
+++ app.js
@@ -0,0 +1,28 @@
+let obj = [
+    {
+    name:"Mesum",
+    contact:"3242342332",
+    institute:"skhakjsHK"
+  },
+  {
+    name:"HAMZA",
+    contact:"3223423243",
+    institute:"QUWASJKDH"
+  },{
+    name:"HASNAIN",
+    contact:"2342344423",
+    institute:"HJWRUW"
+  },{
+    name:"ALI",
+    contact:"3827643334",
+    institute:"LSJFJ"
+  },
+];
+
+let myobj = obj.map(function(x){
+    return x.name;
+    
+});
+
+
+console.log(myobj);
